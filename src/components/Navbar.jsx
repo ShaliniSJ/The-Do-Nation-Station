aecdfe1e1b2bcfb4c3311ba5489b15d20ab94301
Arun Pranav A T <pranav9176@gmail.com>
2024-07-26 11:36:36 +0530

    [arun pranav] bringing back the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
 import { IoSearchSharp } from 'react-icons/io5';
+
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full p-4 md:p-6 flex items-center transition ease-out backdrop-blur-sm bg-primary text-text-light shadow-md">
-      <div>The Do-Nation Station</div>
+    <nav className="fixed top-0 left-0 w-full p-4 md:p-6 flex items-center transition ease-out backdrop-blur-sm bg-primary text-text-light shadow-md">
+      <a href="#">The Do-Nation Station</a>
       <ul className="list-none ml-auto hidden md:flex">
         <li className="ml-5"><a href="#" className="hover:underline focus:underline">About</a></li>
         <li className="ml-5"><a href="#" className="hover:underline focus:underline">Leader Board</a></li>
@@ -13,7 +13,7 @@ const Navbar = () => {
       </ul>
       <div className='mx-2'>
         <button className="bg-blue rounded-full 
-          shadow-[rgba(144,223,245,.2)_0_-25px_18px_-14px_inset,rgba(144,223,245,.15)_0_1px_2px,rgba(144,223,245,.15)_0_2px_4px,rgba(144,223,245,.15)_0_4px_8px,rgba(144,223,245,.15)_0_8px_16px,rgba(144,223,245,.15)_0_16px_32px]
+          shadow-[rgba(144,223,245,.2)0-25px_18px_-14px_inset,rgba(144,223,245,.15)_0_1px_2px,rgba(144,223,245,.15)_0_2px_4px,rgba(144,223,245,.15)_0_4px_8px,rgba(144,223,245,.15)_0_8px_16px,rgba(144,223,245,.15)_0_16px_32px]
           text-gray-200 cursor-pointer
           inline-block font-sans
           py-1.5 px-5
@@ -21,7 +21,7 @@ const Navbar = () => {
           transition-all duration-250 
           border-0
           select-none
-          hover:shadow-[rgba(47,91,253,.35)_0_-25px_18px_-14px_inset,rgba(47,91,253,.25)_0_1px_2px,rgba(47,91,253,.25)_0_2px_4px,rgba(47,91,253,.25)_0_4px_8px,rgba(47,91,253,.25)_0_8px_16px,rgba(47,91,253,.25)_0_16px_32px]
+          hover:shadow-[rgba(47,91,253,.35)0-25px_18px_-14px_inset,rgba(47,91,253,.25)_0_1px_2px,rgba(47,91,253,.25)_0_2px_4px,rgba(47,91,253,.25)_0_4px_8px,rgba(47,91,253,.25)_0_8px_16px,rgba(47,91,253,.25)_0_16px_32px]
           hover:scale-105 hover:rotate-[-1deg] ">
           Login
         </button>

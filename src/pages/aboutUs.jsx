576c23ecd7cfb9b3c4c0e332546ac9452f3640c7
data-overflow <kvrjr27@gmail.com>
2024-07-29 08:30:26 +0530

    [kavi] about us page fix

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import NavbarAfterLogin from '../components/NavbarAfterLogin';
-import NavbarBeforeLogin from '../components/NavbarBeforeLogin';
-import AboutUs from '../components/AboutUsContent';
-import { useState } from 'react';
+import React from "react";
+// import NavbarAfterLogin from '../components/NavbarAfterLogin';
+// import NavbarBeforeLogin from '../components/NavbarBeforeLogin';
+import AboutUs from "../components/AboutUsContent";
+import { useState } from "react";
+import Navbar from "../components/Navbar";
 
-export default function aboutUs(){
-    const [showAfterLogin, setShowBeforeLogin] = useState(true);
+export default function aboutUs() {
+  const [showAfterLogin, setShowBeforeLogin] = useState(true);
 
-    return (
-        <div>
-            {showAfterLogin ? <NavbarBeforeLogin/> : <NavbarAfterLogin />}
-            <AboutUs />
-        </div>
-    );
+  return (
+    <div>
+      {/* {showAfterLogin ? <NavbarBeforeLogin/> : <NavbarAfterLogin />} */}
+      <Navbar />
+      <AboutUs />
+    </div>
+  );
 }

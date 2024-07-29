69ec93f6aa85e0b29dc1fce4c92fdff898a85fd5
data-overflow <kvrjr27@gmail.com>
2024-07-29 11:36:49 +0530

    [kavi] donor responsiveness

--- src/pages/donor/[slug].jsx
+++ src/pages/donor/[slug].jsx
@@ -50,31 +50,32 @@ export default function Page() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col m-6 gap-8">
-        <div className="flex flex-row items-center gap-8">
-          <img
-            className="w-32 h-32 rounded-full"
-            src={donor.avatar_url}
-            alt={donor.name}
-          />
-          <div className="flex flex-col">
-            <p className="text-2xl">{donor.name}</p>
-            <p className="text-xl opacity-80">{donor.user_id}</p>
+      <div className="flex flex-col min-h-[80vh] md:flex-row gap-4 md:gap-16">
+        <div className="flex flex-col bg-blue-200/50 gap-8 p-6 md:pr-16">
+          <div className="flex flex-row items-center gap-8">
+            <img
+              className="w-32 h-32 rounded-full"
+              src={donor.avatar_url}
+              alt={donor.name}
+            />
+            <div className="flex flex-col">
+              <p className="text-2xl">{donor.name}</p>
+              <p className="text-xl opacity-80">{donor.user_id}</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-2">
+            <div className="bg-yellow-100 rounded-md p-2 w-fit">
+              <p className="text-xl">Points: +{donor.total_amount}</p>
+            </div>
+            <div className="bg-green-100 rounded-md p-2 w-fit">
+              <p className="text-xl">Rank: 30</p> {/*TODO*/}
+            </div>
           </div>
         </div>
-
-        <div className="flex flex-row gap-2">
-          <div className="bg-yellow-100 rounded-md p-2 w-fit">
-            <p className="text-xl">Points: +{donor.total_amount}</p>
-          </div>
-          <div className="bg-green-100 rounded-md p-2 w-fit">
-            <p className="text-xl">Rank: 30</p> {/*TODO*/}
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4 mt-8">
-          <h3 className="text-2xl">Past Contributions</h3>
-          <ul className="flex flex-col gap-4 w-fit">{contributionsList}</ul>
+        <div className="flex flex-col gap-4 mt-8 grow p-6">
+          <h3 className="text-3xl font-bold">Past Contributions</h3>
+          <ul className="flex flex-col gap-4 w-full">{contributionsList}</ul>
         </div>
       </div>
     </>

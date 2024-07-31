deb625c68643ac514c95904755f8323c803cb6cd
data-overflow <kvrjr27@gmail.com>
2024-08-01 01:14:15 +0530

    footer responsiveness [kavi]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,25 @@ import BlueLogo from "../assets/blue-image-logo.png";
 export default function Page() {
   return (
     <footer className="flex flex-col justify-between min-h-60 bg-primary-blue text-secondary-blue rounded-3xl m-4 md:m-6 md:mt-16 p-4 md:p-10">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-col md:flex-row justify-between">
         <div className="flex flex-col justify-between gap-4">
-          <div className="flex flex-row">
-            <Image src={IconLogo} alt="The Do-Nation Station Icon" width={43} />
-            <Image src={BlueLogo} alt="The Do-Nation Station" width={250} />
+          <div className="flex flex-col md:flex-row object-scale-down">
+            <Image
+              src={IconLogo}
+              alt="The Do-Nation Station Icon"
+              className="object-scale-down"
+              width={43}
+            />
+            <Image
+              src={BlueLogo}
+              alt="The Do-Nation Station"
+              className="object-scale-down"
+              width={250}
+            />
           </div>
         </div>
-        <div className="flex flex-row justify-between items-start gap-8">
-          <ul className="grid grid-cols-2 gap-x-32 gap-y-2">
+        <div className="flex flex-col md:flex-row justify-between items-start gap-8">
+          <ul className="grid w-full md:w-fit text-center md:text-left grid-cols-1 md:grid-cols-2 gap-x-32 gap-y-4 md:gap-y-2">
             <li>
               <a className="hover:underline" href="/">
                 Home
@@ -45,7 +55,7 @@ export default function Page() {
               </a>
             </li>
           </ul>
-          <div className="w-1/2 opacity-50">
+          <div className="text-center md:text-left nunito md:w-1/2 opacity-50">
             <p>Thank you for your support!</p>
             <p>
               For feedback or to work with us, contact us at{" "}
@@ -57,6 +67,7 @@ export default function Page() {
               </a>
             </p>
           </div>
+          <hr />
         </div>
       </div>
       <p className="text-center jost text-lg opacity-50">

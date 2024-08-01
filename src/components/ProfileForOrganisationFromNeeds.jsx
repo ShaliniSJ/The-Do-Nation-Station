ea73bc522b8740166d8654225c4529d9fcff2e6a
Kierthana <kierthanars@gmail.com>
2024-08-01 19:35:48 +0530

    [kierthana] removed console log

--- src/components/ProfileForOrganisationFromNeeds.jsx
+++ src/components/ProfileForOrganisationFromNeeds.jsx
@@ -109,7 +109,7 @@ const ProfileForOrganisationFromNeeds = () => {
 
   const encodedAddress = encodeURIComponent(orgData.address);
   const mapEmbedLink = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${encodedAddress}`;
-  console.log(currentNeeds)
+  
 
   return (
     <div className="min-h-screen bg-white text-dark-blue p-6">

edda20b00324d10eeaa6b560e54f45d0fc234d9f
data-overflow <kvrjr27@gmail.com>
2024-08-01 21:12:50 +0530

    donate page update [kavi]

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -1,55 +1,55 @@
 // pages/donate.js
-import React, { useState,useEffect } from 'react';
-import { Container, Box, TextField, Button, Typography } from '@mui/material';
-import { useRouter } from 'next/router';
-
+import React, { useState, useEffect } from "react";
+import { Container, Box, TextField, Button, Typography } from "@mui/material";
+import { useRouter } from "next/router";
 
 const Donate = () => {
-  const [amount, setAmount] = useState('');
-  const [needId,setNeedId]=useState('')
+  const [amount, setAmount] = useState("");
+  const [needId, setNeedId] = useState("");
   const router = useRouter();
   useEffect(() => {
     const fetchData = async () => {
       const url = new URL(window.location.href);
       const userId = url.href.split("?").pop();
-      setNeedId(userId)
-     
-    }
+      setNeedId(userId);
+    };
     fetchData();
-  },[])
+  }, []);
 
   const handleDonate = () => {
     router.push(`/carddetails?amount=${amount}id=${needId}`);
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box sx={{ mt: 8, textAlign: 'center' }}>
-        <Typography component="h1" variant="h5">
-          Enter Donation Amount
-        </Typography>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="amount"
-          label="Amount"
-          name="amount"
-          autoFocus
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <Button
-          type="button"
-          fullWidth
-          variant="contained"
-          color="primary"
-          onClick={handleDonate}
-        >
-          Donate
-        </Button>
-      </Box>
-    </Container>
+    <div className="flex flex-col justify-center items-center min-h-[90vh]">
+      <Container component="main" maxWidth="xs">
+        <Box sx={{ mt: 8, textAlign: "center" }}>
+          <Typography component="h1" variant="h5">
+            Enter Donation Amount
+          </Typography>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="amount"
+            label="Amount"
+            name="amount"
+            autoFocus
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleDonate}
+          >
+            Donate
+          </Button>
+        </Box>
+      </Container>
+    </div>
   );
 };
 

e40d7da2a3b481cc00894d0525e20cbb508ce7e2
ShaliniSJ <s.j.shalini2005@gmail.com>
2024-08-22 05:23:40 +0530

    [Shalini]uncomments isLogged in organisation details

--- src/pages/organisationprofile.jsx
+++ src/pages/organisationprofile.jsx
@@ -5,27 +5,27 @@ import HomeWithOutLogin from '../components/HomeWithOutLogin';
 import ProfileForOrganisation from '../components/ProfileForOrganisation';
 
 export default function Home() {
-  // const [showAfterLogin, setShowAfterLogin] = useState(false);
-  // const [user, setUser] = useState(null);
-  // const [isLogged, setIsLogged] = useState(false);
-  // const [isdonor,setIsdonor]=useState(false);
+  const [showAfterLogin, setShowAfterLogin] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLogged, setIsLogged] = useState(false);
+  const [isdonor,setIsdonor]=useState(false);
   
     
-  // useEffect(() => {
-  //   const fetchUserData = async () => {
-  //     if (typeof window !== 'undefined') {
-  //       const islogged = localStorage.getItem('islogged');
-  //       if(islogged === 'true') {
-  //         setIsLogged(true);
-  //       }
-  //       else{
-  //         setIsLogged(false);
-  //       }
-  //     }
-  //   };
-  //   // Call the async function
-  //   fetchUserData();
-  // }, [isLogged]);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (typeof window !== 'undefined') {
+        const islogged = localStorage.getItem('islogged');
+        if(islogged === 'true') {
+          setIsLogged(true);
+        }
+        else{
+          setIsLogged(false);
+        }
+      }
+    };
+    // Call the async function
+    fetchUserData();
+  }, [isLogged]);
 
   return (
     <div>

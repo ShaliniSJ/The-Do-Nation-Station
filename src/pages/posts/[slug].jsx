10f3c1e83a445f9e06acddd8d9954d4abbba1612
Kierthana <kierthanars@gmail.com>
2024-08-22 03:35:57 +0530

    [kierthana] Removed the duplicate Navbar from posts page

--- src/pages/posts/[slug].jsx
+++ src/pages/posts/[slug].jsx
@@ -1,4 +1,4 @@
-import Navbar from "@/src/components/Navbar";
+// import Navbar from "@/src/components/Navbar";
 import {
   getComments,
   createComment,
@@ -128,7 +128,7 @@ export default function Page() {
 
   return (
     <>
-      <Navbar islogged={isloggedin} />
+      {/* <Navbar islogged={isloggedin} /> */}
       <div className="flex flex-col gap-8">
         <div
           key={post.$id}

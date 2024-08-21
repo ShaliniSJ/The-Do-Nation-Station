6ba091ae15ac46c5de0fd22fea4afb60fc97f53c
ShaliniSJ <s.j.shalini2005@gmail.com>
2024-08-21 09:45:01 +0530

    [kierthana]adds navbar in explore

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,9 +1,28 @@
 import Navbar from "../components/Navbar"
 import ExploreTab from "../components/ExploreTab"
+import { useState,useEffect } from 'react';
+
 const explore = () => {
+    const [isLogged, setIsLogged] = useState(false);
+    useEffect(() => {
+        // Define an async function to handle the async operation
+        const fetchUserData = async () => {
+          if (typeof window !== 'undefined') {
+            const islogged = localStorage.getItem('islogged');
+            if(islogged === 'true') {
+              setIsLogged(true);
+            }
+            else{
+              setIsLogged(false);
+            }
+          }
+        };
+        fetchUserData();
+        }
+        , [isLogged]);
     return (
         <div>
-            <Navbar />
+            <Navbar  islogged={isLogged} />
             <ExploreTab/>
         </div>
     )
